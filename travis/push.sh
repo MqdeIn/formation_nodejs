7e006e4815db8b349c4e616f4306157661c2ce74
hudault francois <mqde@protonmail.com>
2017-12-07 11:59:47 +0100

    epzaojg

--- travis/push.sh
+++ travis/push.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "toto"
-git remote add clerverapps
-htps://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/formation_nodejs.git
-git --verbose --force pusg ckervapps master
+git remote add cleverapps https://ba3ec38362d94ffd973079492503b198:82feba109cc4486b9bcb7e1839861dff@push-par-clevercloud-customers.services.clever-cloud.com/app_7f9c1411-545d-4c3f-8366-47b95bf0f87f.git
+git push cleverapps master
