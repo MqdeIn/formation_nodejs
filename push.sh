66052c567f478721a9547b86dc1ae51bcfec54b4
hudault francois <mqde@protonmail.com>
2017-12-07 11:05:07 +0100

    lazdkza

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git remote add clerverapps
-htps://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_formation_nodejs.git
+htps://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/formation_nodejs.git
 git --verbose --force pusg ckervapps master
