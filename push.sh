e9354fb1141ed8795ec9d5537a4c4261eafe3813
hudault francois <mqde@protonmail.com>
2017-12-07 11:50:29 +0100

    epzaojg

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git remote add cleverapps htps://$CLEVER_TOKEN:$CLEVER_SECRET@push-par-clevercloud-customers.services.clever-cloud.com/app_7f9c1411-545d-4c3f-8366-47b95bf0f87f.git
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push-par-clevercloud-customers.services.clever-cloud.com/app_7f9c1411-545d-4c3f-8366-47b95bf0f87f.git
 git push cleverapps master
